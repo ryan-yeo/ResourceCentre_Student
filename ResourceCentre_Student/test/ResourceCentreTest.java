be2a4d5317729c111442bfbf92869d32dd6e889c
19026268 <19026268@DESKTOP-TRLLT6J>
2020-07-24 15:34:14 +0800

    ResourceTest add

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -33,25 +33,41 @@ public class ResourceCentreTest {
 
 	@Test
 	public void addCamcorderTest() {
+	    
 		// Item list is not null, so that can add a new item
-		assertNotNull("Test if there is valid Camcorder arraylist to add to", camcorderList);
-		
-		//Given an empty list, after adding 1 item, the size of the list is 1
-		ResourceCentre.addCamcorder(camcorderList, cc1);		
-		assertEquals("Test if that Camcorder arraylist size is 1?", 1, camcorderList.size());
-		
-		//The item just added is as same as the first item of the list
-		assertSame("Test that Camcorder is added same as 1st item of the list?", cc1, camcorderList.get(0));
-		
-		//Add another item. test The size of the list is 2?
-		ResourceCentre.addCamcorder(camcorderList, cc2);
-		assertEquals("Test that Camcorder arraylist size is 2?", 2, camcorderList.size());
-	}
+	    assertNotNull("Test if there is valid Camcorder arraylist to add to", camcorderList);
+
+	    // Given an empty list, after adding 1 item, the size of the list is 1
+	    ResourceCentre.addCamcorder(camcorderList, cc1);
+	    assertEquals("Test if that Camcorder arraylist size is 1?", 1, camcorderList.size());
+
+	    // The item just added is as same as the first item of the list
+	    assertSame("Test that Camcorder is added same as 1st item of the list?", cc1, camcorderList.get(0));
+
+	    // Add another item. test The size of the list is 2?
+	    ResourceCentre.addCamcorder(camcorderList, cc2);
+	    assertEquals("Test that Camcorder arraylist size is 2?", 2, camcorderList.size());
+	    } 
 	
 	@Test
 	public void addChromebookTest() {
-		//fail("Not yet implemented");
-		// write your code here
+
+	     // NICOLE
+	     // fail("Not yet implemented"); 
+	     // Item list is not null, so that can add a new item
+	     assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+
+	     // Given an empty list, after adding 1 item, the size of the list is 1
+	     ResourceCentre.addChromebook(chromebookList, cb1);
+	     assertEquals("Test if that Chromebook arraylist size is 1?", 1, chromebookList.size());
+
+	     // The item just added is as same as the first item of the list
+	     assertSame("Test that Chromeboook is added same as 1st item of the list?", cb1, chromebookList.get(0));
+
+	     // Add another item. test The size of the list is 2?
+	     ResourceCentre.addChromebook(chromebookList, cb2);
+	     assertEquals("Test that Chromebook arraylist size is 2?", 2, chromebookList.size());
+	    }
 	}
 	
 	@Test
