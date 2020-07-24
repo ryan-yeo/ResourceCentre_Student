ca9ee7d8a8b3fbfa4744331d8e7d9fe823d723cb
19047433 <19047433@DESKTOP-TDL3545>
2020-07-24 13:42:47 +0800

    resourceCenter chromebook add part nicole

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -175,12 +175,18 @@ public class ResourceCentre {
 		public static Chromebook inputChromebook() {
 			Chromebook cb = null;
 			// write your code here
+			String tag = Helper.readString("Enter asset tag > ");
+			String description = Helper.readString("Enter description > ");
+			String os = Helper.readString("Enter OS > ");
+			cb = new Chromebook(tag, description, os);
 			return cb;
 
 		}
 
 		public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 			// write your code here
+			chromebookList.add(cb);
+			System.out.println("Chromebook added");
 		}
 	
 	//================================= Option 3 Loan =================================
