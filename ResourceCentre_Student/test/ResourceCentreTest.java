346ebdd1a725ae9d4a92fc2326670ae7b3bdaf26
19045083 <19045083@LAPTOP-A3SQDH7M.mshome.net>
2020-07-24 12:42:54 +0800

    RetrieveAllChromebookTest() in ResourceCentre, completed but cant test due to addChromebook not done.

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -58,11 +58,12 @@ public class ResourceCentreTest {
 	public void retrieveAllCamcorderTest() {
 		// Test if Item list is not null but empty, so that can add a new item
 		assertNotNull("Test if there is valid Camcorder arraylist to add to", camcorderList);
-		
+
 		//test if the list of camcorders retrieved from the SourceCentre is empty
 				String allCamcorder= ResourceCentre.retrieveAllCamcorder(camcorderList);
 				String testOutput = "";
 				assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
+				
 		//Given an empty list, after adding 2 items, test if the size of the list is 2
 		ResourceCentre.addCamcorder(camcorderList, cc1);
 		ResourceCentre.addCamcorder(camcorderList, cc2);
